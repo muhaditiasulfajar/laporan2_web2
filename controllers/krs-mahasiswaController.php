c13cf7889935b98357731ceed1b9d779c61e2856
Your Name <muhaditiasulfajar@gamil.com>
2023-05-09 13:31:10 +0800

    first commit

---
+++ controllers/krs-mahasiswaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class krs-mahasiswaController extends \yii\web\Controller
+{
+    public function actionManagemenKrs()
+    {
+        return $this->render('managemen-krs');
+    }
+
+}
---
+++ views/daftar-matakuliah/update.php
@@ -0,0 +1,11 @@
+<?php
+/** @var yii\web\View $this */
+?>
+<h1>daftar-matakuliah/update</h1>
+<h2>Tampilan Index</h2>
+<h3>muh.Adita sulfajar </h3>
+
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
